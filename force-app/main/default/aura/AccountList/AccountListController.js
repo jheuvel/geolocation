daae92e647e214c14f9280b494497962c070daa0
Jochem van den Heuvel <jochem@screenfields.nl>
2018-07-05 15:24:52 +0200

    Update

--- force-app/main/default/aura/AccountList/AccountListController.js
+++ force-app/main/default/aura/AccountList/AccountListController.js
@@ -3,7 +3,9 @@
         var action = component.get("c.findAll");
         action.setCallback(this, function(a) {
             component.set("v.accounts", a.getReturnValue());
-            var event = $A.get("e.c:AccountsLoaded");
+            //var event = $A.get("e.c:AccountsLoaded");
+            var event = component.getEvent("AccountsLoaded");
+
             event.setParams({"accounts": a.getReturnValue()});
             event.fire();
         });
